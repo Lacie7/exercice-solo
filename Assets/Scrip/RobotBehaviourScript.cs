5b885fd8d07e2050ff90a1f84c9d24d40d7a6f38
Mitsuki <e.duran@etudiant.ecolemelies.fr>
2020-06-18 17:22:36 +0200

    correction script

--- Assets/Scrip/RobotBehaviourScript.cs
+++ Assets/Scrip/RobotBehaviourScript.cs
@@ -37,7 +37,7 @@ public class RobotBehaviourScript : MonoBehaviour
         
     }
     // Update is called once per frame
-    void FixeUpdate()
+    void FixedUpdate()
     {
         var myRigidBody = GetComponent<Rigidbody2D>();
         //direction.y = 0;
