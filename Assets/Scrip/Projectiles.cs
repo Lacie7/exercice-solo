3189aced34f4491b472b473a566c18a05485166c
Mitsuki <e.duran@etudiant.ecolemelies.fr>
2020-06-20 17:25:42 +0200

    add script projectiles

---
+++ Assets/Scrip/Projectiles.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectiles : MonoBehaviour
+
+    
+{
+    [SerializeField] private float speed;
+
+    private Rigidbody myRigidbody;
+
+    private void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody>();
+       
+        var RobotGameObject = GameObject.FindWithTag("Player");
+        var Projectiles = RobotGameObject.GetComponent<Projectiles>();
+        RobotBehaviourScript.onDeath.AddListener(Die);
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        myRigidbody.velocity = new Vector3
+        {
+            x = speed * Time.fixedDeltaTime,
+            y = 0,
+            z = 0
+        };
+    }
+
+    private void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
+
+    private void OnCollisionEnter()
+    {
+        Destroy(gameObject);
+    }
+
+    private void Die()
+    {
+        Destroy(gameObject);
+    }
+}
